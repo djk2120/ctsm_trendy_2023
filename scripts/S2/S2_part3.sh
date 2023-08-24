8847676cb9a88dd5149072dc78d4d6ce8d92a22f
Daniel Kennedy <djk2120@columbia.edu>
2023-08-24 10:24:28 -0600

    actually running S2

--- scripts/S2/S2_part3.sh
+++ scripts/S2/S2_part3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ## run the last bit (1901-2022)
 ## bring in transient climate
-casedir='/glade/u/home/djk2120/ctsm_trendy_2022/sims/'
+casedir='/glade/u/home/djk2120/ctsm_trendy_2023/sims/'
 exp='S2'
-casename='TRENDY2022_f09_'$exp
+casename='TRENDY2023_f09_'$exp
 caseroot=$casedir$casename
 
 cd $caseroot
